76474f1fdf720c412aef1bc837706e38daed04b0
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2022-08-18 18:16:56 -0300

    timers das cartas

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-let gifs = ['./imagens/bobrossparrot.gif', './imagens/explodyparrot.gif', './imagens/fiestaparrot.gif', './imagens/metalparrot.gif', './imagens/revertitparrot.gif', './imagens/tripletsparrot.gif', './imagens/unicornparrot.gif']
-
-
-
-
+let gifs = ['./imagens/bobrossparrot.gif', './imagens/bobrossparrot.gif', './imagens/explodyparrot.gif', './imagens/explodyparrot.gif', './imagens/fiestaparrot.gif', './imagens/fiestaparrot.gif', './imagens/metalparrot.gif', './imagens/metalparrot.gif', './imagens/revertitparrot.gif', './imagens/revertitparrot.gif', './imagens/tripletsparrot.gif', './imagens/tripletsparrot.gif', './imagens/unicornparrot.gif', './imagens/unicornparrot.gif']
+let arr = []
 
 function Inicio() {
 
   const pergunta = (prompt("com quantas cartas deseja jogar ?, entre 4 e 14, e tem que ser número par :)"))
   let lista = document.querySelector('ul')
-
-  gifs.sort(comparador)
-
   if (pergunta >= 4 && pergunta % 2 === 0 && pergunta <= 14) {
     for (let i = 0; pergunta > i; i++) {
-      
+      arr.push(gifs[i])
+    }
+    arr.sort(comparador)
+    for (let i = 0; pergunta > i; i++) {
+
+
       lista.innerHTML += ` <div class="card" onclick="cartaClique(this)">
-            <div class="front-face face">
+            <div class="papagaio face">
             <img src="./imagens/front.png" alt="">
             </div>
-            <div class="back-face face">
-            <img src="${gifs[i]}">
+            <div class="gif face">
+            <img src="${arr[i]}">
             </div>
         </div>`
     }
@@ -35,13 +34,23 @@ Inicio()
 
 function cartaClique(carta) {
 
-  const back = carta.querySelector(".back-face")
-  back.classList.toggle("back-click")
+  let papagaio = carta.querySelector(".papagaio")
+  let gif = carta.querySelector(".gif")
 
-  const front = carta.querySelector(".front-face")
-  front.classList.toggle("front-click")
+  papagaio.classList.add("front-click")
+  gif.classList.add("back-click")
 
+  setTimeout(virarCarta,1000,carta)
 }
+
+function virarCarta(carta){
+  let papagaio = carta.querySelector(".papagaio")
+  let gif = carta.querySelector(".gif")
+
+  papagaio.classList.remove("front-click")
+  gif.classList.remove("back-click")
+}
+
 
 function comparador() {
   return Math.random() - 0.5;
