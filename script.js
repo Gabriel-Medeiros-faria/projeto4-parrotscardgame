90fb680b7f93a63ca2220ad771e84f4f6819c792
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2022-08-22 15:42:37 -0300

    quase terminado

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// Adicionar classe block nas duas que estiverem certas
 
 let gifs = ['./imagens/bobrossparrot.gif', './imagens/bobrossparrot.gif', './imagens/explodyparrot.gif', './imagens/explodyparrot.gif', './imagens/fiestaparrot.gif', './imagens/fiestaparrot.gif', './imagens/metalparrot.gif', './imagens/metalparrot.gif', './imagens/revertitparrot.gif', './imagens/revertitparrot.gif', './imagens/tripletsparrot.gif', './imagens/tripletsparrot.gif', './imagens/unicornparrot.gif', './imagens/unicornparrot.gif']
 let arr = []
 let arrDeDois = []
 let quantidadeJogadas=0;
+let pergunta = 0
+let acertos = 0
+let podeVirarCartas = true
+
 
 function Inicio() {
 
-  const pergunta = (prompt("com quantas cartas deseja jogar ?, entre 4 e 14, e tem que ser número par :)"))
+  pergunta = (prompt("com quantas cartas deseja jogar ?, entre 4 e 14, e tem que ser número par :)"))
   let lista = document.querySelector('ul')
   if (pergunta >= 4 && pergunta % 2 === 0 && pergunta <= 14) {
     for (let i = 0; pergunta > i; i++) {
@@ -17,7 +20,7 @@ function Inicio() {
     for (let i = 0; pergunta > i; i++) {
 
 
-      lista.innerHTML += ` <div class="card" onclick="cartaClique(this)">
+      lista.innerHTML += `<div class="card" onclick="cartaClique(this)">
             <div class="papagaio face">
             <img src="./imagens/front.png" alt="">
             </div>
@@ -34,15 +37,17 @@ function Inicio() {
 Inicio()
 
 function cartaClique(carta) {
+  if(carta.classList.contains('acertada')===true){
+    return
+  }
 
   quantidadeJogadas=quantidadeJogadas+1
-  console.log(quantidadeJogadas)
   let papagaio = carta.querySelector(".papagaio")
   let gif = carta.querySelector(".gif")
 
   papagaio.classList.add("front-click")
   gif.classList.add("back-click")
-
+  
   verificarPar(carta)
 }
 
@@ -68,7 +73,6 @@ function verificarPar(carta) {
   }
   else {
     arrDeDois.push(carta)
-    console.log(arrDeDois)
     let primeiroItemArray = arrDeDois[0]
     let primeiroItem = primeiroItemArray.querySelector('.image').src
 
@@ -78,33 +82,63 @@ function verificarPar(carta) {
 
     
     if (primeiroItem === segundoItem) {
-      carta.classList.add('acertada')
-      
+      arrDeDois[0].classList.add('acertada')
+      arrDeDois[1].classList.add('acertada')
+      acertos=acertos+2
+    
+      checarFinalJogo()
     }
 
     else {
       setTimeout(virarCarta, 1000, primeiroItemArray)
       setTimeout(virarCarta, 1000, segundoItemArray)
-      //caso não der certo
     }
     arrDeDois = []
-
+    
   }
+}
 
-
+function checarFinalJogo(){
+  console.log('entrou na função')
+  if(arr.length===acertos){
+    
+    setTimeout(finalAlert, 1000)
+  }
+} 
+function finalAlert(){
+  alert("Você ganhou em " + quantidadeJogadas + " jogadas!")
+  let Perguntadnv = prompt("Gostaria de jogar novamente?")
+  if(Perguntadnv === "sim"){
+    repeteInicio()
+  }
+  else{
+    alert("Obrigado por jogar :) ")
+  }
 
 }
 
-/* function checarFinalJogo(){
-  let contador = 0
+function repeteInicio(){
+  pergunta = (prompt("com quantas cartas deseja jogar ?, entre 4 e 14, e tem que ser número par :)"))
+  let lista = document.querySelector('ul')
+  if (pergunta >= 4 && pergunta % 2 === 0 && pergunta <= 14) {
+    for (let i = 0; pergunta > i; i++) {
+      arr.push(gifs[i])
+    }
+    arr.sort(comparador)
+    for (let i = 0; pergunta > i; i++) {
 
-  for(i=0;i<arrDeDois.length;i++){
-    if(arrDeDois[i].classList.contains('acertada')===true){
-      contador++
+
+      lista.innerHTML += `<div class="card" onclick="cartaClique(this)">
+            <div class="papagaio face">
+            <img src="./imagens/front.png" alt="">
+            </div>
+            <div class="gif face">
+            <img class="image" src="${arr[i]}">
+            </div>
+        </div>`
     }
   }
-  if(arrDeDois.length===contador){
-    alert("Você ganhou em " + quantidadeJogadas + " jogadas !")
-    Inicio()
+  else {
+    repeteInicio()
   }
 }
